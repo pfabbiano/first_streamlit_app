ddd1c102f1855fdca45a38f2f5f4f8b14b266bc4
Pablo Fabbiano <39975206+pfabbiano@users.noreply.github.com>
2022-07-18 12:31:48 -0300

    Update streamlit_app.py
    Nuevos emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ streamlit.title('My parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
