a44ebf29aa7864c09ebadd86a98f20dd75a1d3d1
Pablo Fabbiano <39975206+pfabbiano@users.noreply.github.com>
2022-07-18 12:21:38 -0300

    Update streamlit_app.py
    Enciclopedia de Emijis.

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.text('Thank you very much! and yes I've never used emojis before in my work, I even haven't been aware of Emojipedia/Emojuguide 😝')
+
+
