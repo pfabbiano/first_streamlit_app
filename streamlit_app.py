7b1eabd6649c526614d0fc906849e84d6cddb025
Pablo Fabbiano <39975206+pfabbiano@users.noreply.github.com>
2022-07-18 12:17:47 -0300

    Update streamlit_app.py
    Otra de las modificaciones... La tercera creo...

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title('My parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Opcion 1')
-streamlit.text('Opcion 2')
-streamlit.text('Opcion 3')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
