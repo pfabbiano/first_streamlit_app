4d0d7c8dbfeefe5b83bcea5e7b828aa7e693c5b7
Pablo Fabbiano <39975206+pfabbiano@users.noreply.github.com>
2022-07-18 12:08:42 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Opcion 1')
+streamlit.text('Opcion 2')
+streamlit.text('Opcion 3')
