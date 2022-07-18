73be6adb29e18bf9351e4d306fa3eba64ad794e5
Pablo Fabbiano <39975206+pfabbiano@users.noreply.github.com>
2022-07-18 13:06:47 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,26 @@
 import streamlit
-import pandas
 
 streamlit.title('My parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.header('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
+# streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+# streamlit.header('\N{flexed biceps} Breakfast of Champion Coders \N{flexed biceps}')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+# streamlit.text('Omega 3 & Blueberry Oatmeal')
+# streamlit.text('Kale, Spinach & Rocket Smoothie')
+# streamlit.text('Hard-Boiled Free-Range Egg')
 
-streamlit.text('😝 Thank you very much! and yes I have never used emojis before in my work')
-streamlit.text('I even have not been aware of Emojipedia/Emojuguide')
+# streamlit.text('😝 Thank you very much! and yes I have never used emojis before in my work')
+# streamlit.text('I even have not been aware of Emojipedia/Emojuguide')
 
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+# streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
+
